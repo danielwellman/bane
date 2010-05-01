414e488a895716143ba3884b634da4b3f0e280c6
Daniel Wellman <etldan@gmail.com>
2010-05-01 16:45:26 -0400

    Adding an example of the way to specify options.  Updating TODO.

---
+++ examples/specify_behavior_options.rb
@@ -0,0 +1,15 @@
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
+require 'bane'
+
+include Bane
+include Behaviors
+
+launcher = Launcher.new(
+  10256 => { :behavior => CloseAfterPause, :duration => 3 },
+  10689 => { :behavior => SlowResponse, :message => "Custom message", :pause_duration => 15 },
+  11239 => CloseAfterPause # Use the defaults for this behavior, don't need a Hash
+
+)
+launcher.start
+launcher.join
+# runs until interrupt...
