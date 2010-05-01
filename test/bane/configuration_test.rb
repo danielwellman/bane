49a5f34e0f5cfcad00b1fcdfa65076c216621688
Daniel Wellman <etldan@gmail.com>
2010-05-01 11:29:16 -0400

    Removing unneeded Bane qualification since Bane is included

--- test/bane/configuration_test.rb
+++ test/bane/configuration_test.rb
@@ -10,15 +10,15 @@ class ConfigurationTest < Test::Unit::TestCase
     assert_equal 1, configuration.to_a.size, "Should have created one configuration"
 
     actual_entry = configuration.to_a[0]
-    assert_configuration(actual_entry, :port => 3000, :behavior => Bane::Behaviors::CloseAfterPause)
+    assert_configuration(actual_entry, :port => 3000, :behavior => Behaviors::CloseAfterPause)
   end
 
   def test_should_map_multiple_servers_given_one_starting_port
-    configuration = Bane::Configuration.new(3000, "CloseAfterPause", "CloseImmediately")
+    configuration = Configuration.new(3000, "CloseAfterPause", "CloseImmediately")
     assert_equal 2, configuration.to_a.size, "Should have created two configurations"
 
-    assert_configuration(configuration.to_a[0], :port => 3000, :behavior => Bane::Behaviors::CloseAfterPause)
-    assert_configuration(configuration.to_a[1], :port => 3001, :behavior => Bane::Behaviors::CloseImmediately)
+    assert_configuration(configuration.to_a[0], :port => 3000, :behavior => Behaviors::CloseAfterPause)
+    assert_configuration(configuration.to_a[1], :port => 3001, :behavior => Behaviors::CloseImmediately)
   end
 
   def test_should_ask_service_registry_for_all_behaviors_if_none_specified
@@ -26,7 +26,7 @@ class ConfigurationTest < Test::Unit::TestCase
     another_fake_behavior = Module.new
 
     ServiceRegistry.expects(:all_servers).returns([fake_behavior, another_fake_behavior])
-    configuration = Bane::Configuration.new(3000)
+    configuration = Configuration.new(3000)
     assert_equal 2, configuration.to_a.size, "Should have created two configurations"
 
     assert_configuration(configuration.to_a[0], :port => 3000, :behavior => fake_behavior)
@@ -35,7 +35,7 @@ class ConfigurationTest < Test::Unit::TestCase
 
   def test_should_raise_exception_if_no_arguments
     assert_raises ArgumentError do
-      Bane::Configuration.new
+      Configuration.new
     end
   end
 
