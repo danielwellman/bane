5c293b18d767b1d7c1a07b1a6af2636bd5d2a563
Daniel Wellman <etldan@gmail.com>
2010-05-01 11:40:50 -0400

    Making a nicer test helper for configuration Adding IDEA custom spelling dictionary

--- test/bane/configuration_test.rb
+++ test/bane/configuration_test.rb
@@ -7,18 +7,17 @@ class ConfigurationTest < Test::Unit::TestCase
 
   def test_should_map_single_port_and_server_name
     configuration = Bane::Configuration.new(3000, "CloseAfterPause")
-    assert_equal 1, configuration.to_a.size, "Should have created one configuration"
-
-    actual_entry = configuration.to_a[0]
-    assert_configuration(actual_entry, :port => 3000, :behavior => Behaviors::CloseAfterPause)
+    assert_matches_configuration([
+            {:port => 3000, :behavior => Behaviors::CloseAfterPause}
+    ], configuration)
   end
 
   def test_should_map_multiple_servers_given_one_starting_port
-    configuration = Configuration.new(3000, "CloseAfterPause", "CloseImmediately")
-    assert_equal 2, configuration.to_a.size, "Should have created two configurations"
-
-    assert_configuration(configuration.to_a[0], :port => 3000, :behavior => Behaviors::CloseAfterPause)
-    assert_configuration(configuration.to_a[1], :port => 3001, :behavior => Behaviors::CloseImmediately)
+    configuration = Configuration.new(3000, "CloseImmediately", "CloseAfterPause")
+    assert_matches_configuration([
+            {:port => 3000, :behavior => Behaviors::CloseImmediately},
+            {:port => 3001, :behavior => Behaviors::CloseAfterPause}
+    ], configuration)
   end
 
   def test_should_ask_service_registry_for_all_behaviors_if_none_specified
@@ -26,11 +25,11 @@ class ConfigurationTest < Test::Unit::TestCase
     another_fake_behavior = Module.new
 
     ServiceRegistry.expects(:all_servers).returns([fake_behavior, another_fake_behavior])
-    configuration = Configuration.new(3000)
-    assert_equal 2, configuration.to_a.size, "Should have created two configurations"
-
-    assert_configuration(configuration.to_a[0], :port => 3000, :behavior => fake_behavior)
-    assert_configuration(configuration.to_a[1], :port => 3001, :behavior => another_fake_behavior)
+    configuration = Configuration.new(4000)
+    assert_matches_configuration([
+            {:port => 4000, :behavior => fake_behavior},
+            {:port => 4001, :behavior => another_fake_behavior}
+    ], configuration)
   end
 
   def test_should_raise_exception_if_no_arguments
@@ -39,11 +38,20 @@ class ConfigurationTest < Test::Unit::TestCase
     end
   end
 
+  def test_should_map_options_as_port_and_behavior
+
+  end
+
   private
 
-  def assert_configuration(entry, options)
-    assert_equal options[:port], entry[0], "Wrong port"
-    assert_equal options[:behavior], entry[1], "Wrong behavior"
+  def assert_matches_configuration(expected_config, actual_config)
+    actual_elements = actual_config.to_a
+    assert_equal expected_config.size, actual_elements.size, "Did not create correct number of configurations. Actual: #{actual_elements}, expected #{expected_config}"
+
+    expected_config.each_with_index do |expected, index|
+      assert_equal expected[:port], actual_elements[index][0], "Wrong port"
+      assert_equal expected[:behavior], actual_elements[index][1], "Wrong behavior"
+    end
   end
 
 end
