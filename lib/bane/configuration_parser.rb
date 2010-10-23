0f476aeef611e0f2d03336c11cd7537d365c9bfb
Daniel Wellman <etldan@gmail.com>
2010-10-23 17:47:40 -0400

    Moving the configuration exceptions and helper method into configuration_parser.rb, and deleting configuration.rb.

--- lib/bane/configuration_parser.rb
+++ lib/bane/configuration_parser.rb
@@ -74,4 +74,20 @@ module Bane
 
   end
 
+
+  class ConfigurationError < RuntimeError; end
+
+  class UnknownBehaviorError < RuntimeError
+    def initialize(name)
+      super "Unknown behavior: #{name}"
+    end
+  end
+
+end
+
+# Helper method to easily create configuration.
+module Kernel
+  def Configuration(*args)
+    Bane::ConfigurationParser.new(*args).configurations
+  end
 end
