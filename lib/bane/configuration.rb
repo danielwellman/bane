832df62758a47b7979df6f73944e5221db37f0f5
Daniel Wellman <etldan@gmail.com>
2012-08-11 22:34:20 -0400

    Moving class method body into an instance method, keeping the factory method.

--- lib/bane/configuration.rb
+++ lib/bane/configuration.rb
@@ -3,7 +3,16 @@ require 'optparse'
 module Bane
   class Configuration
   	def self.from(args)
-      options = { :host => BehaviorServer::DEFAULT_HOST }
+      config = self.new(args)
+      results = config.parse
+  	end
+
+    def initialize(args)
+      @args = args
+    end
+
+    def parse
+     options = { :host => BehaviorServer::DEFAULT_HOST }
 
       OptionParser.new do |opts|
         opts.banner = "Usage: bane [options] port [behaviors]"
@@ -11,41 +20,40 @@ module Bane
           "Listen on HOST_ADDRESS.  Defaults to (#{BehaviorServer::DEFAULT_HOST})") do |host|
           options[:host] = host
         end
-      end.parse!(args)
+      end.parse!(@args)
 
-      if (args.size == 1)
-        LinearPortMappedBehaviorConfiguration.new(Integer(args[0]), ServiceRegistry.all_servers, options[:host])
-      elsif (args.size >= 2)
-        behaviors = args.drop(1).map { |behavior| find(behavior) }
-        LinearPortMappedBehaviorConfiguration.new(Integer(args[0]), behaviors, options[:host])
+      if (@args.size == 1)
+        LinearPortMappedBehaviorConfiguration.new(Integer(@args[0]), ServiceRegistry.all_servers, options[:host])
+      elsif (@args.size >= 2)
+        behaviors = @args.drop(1).map { |behavior| find(behavior) }
+        LinearPortMappedBehaviorConfiguration.new(Integer(@args[0]), behaviors, options[:host])
       else
         raise "nothing"
       end
-  	end
+    end
 
     private 
-    def self.find(behavior)
+
+    def find(behavior)
       Behaviors.const_get(behavior)      
     end
-  end
 
-  private 
-
-  class LinearPortMappedBehaviorConfiguration
-    def initialize(port, behaviors, host)
-      @starting_port = port
-      @behaviors = behaviors
-      @host = host
-    end
-
-    def servers
-      configurations = []
-      @behaviors.each_with_index do |behavior, index|
-        configurations << BehaviorServer.new(@starting_port + index, @host, behavior.new)
+    class LinearPortMappedBehaviorConfiguration
+      def initialize(port, behaviors, host)
+        @starting_port = port
+        @behaviors = behaviors
+        @host = host
       end
-      configurations
+
+      def servers
+        configurations = []
+        @behaviors.each_with_index do |behavior, index|
+          configurations << BehaviorServer.new(@starting_port + index, @host, behavior.new)
+        end
+        configurations
+      end
+
     end
 
   end
-
 end
