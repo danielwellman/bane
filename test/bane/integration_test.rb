f930da96e1c4658e679cc4ce8f215b18ae8bda92
Daniel Wellman <etldan@gmail.com>
2012-08-18 14:19:31 -0400

    Adding an integration test for command line configuration.
    Adding a temporary sleep until I can resolve Issue #7.

--- test/bane/integration_test.rb
+++ test/bane/integration_test.rb
@@ -34,26 +34,41 @@ class BaneIntegrationTest < Test::Unit::TestCase
         assert_match /401/, e.message
       end
     end
+  end
 
+  def test_supports_command_line_interface
+    run_server_with_cli_arguments(["--listen-on-localhost", TEST_PORT, "FixedResponse"]) do
+      connect_to TEST_PORT do |response|
+        assert !response.empty?, "Should have had a non-empty response"
+      end
+    end
   end
 
   private
 
-  def run_server_with(port, behavior)
+  def run_server_with(port, behavior, &block)
     behavior = Bane::BehaviorServer.new(port, behavior.new)
     launcher = Bane::Launcher.new([behavior], quiet_logger)
-    launcher.start
-    yield
-    ensure
-      launcher.stop if launcher
+    launch_and_stop_safely(launcher, &block)
   end
 
-  def run_server_with_deprecated(*options)
+  def run_server_with_deprecated(*options, &block)
     launcher = Bane::Launcher.new(Configuration(*options), quiet_logger)
+    launch_and_stop_safely(launcher, &block)
+  end
+
+  def run_server_with_cli_arguments(arguments, &block)
+    config = Bane::CommandLineConfiguration.new()
+    launcher = Bane::Launcher.new(config.parse(arguments), quiet_logger)
+    launch_and_stop_safely(launcher, &block)
+  end
+
+  def launch_and_stop_safely(launcher, &block)
     launcher.start
-    yield
+    block.call
     ensure
       launcher.stop if launcher
+      sleep 0.1 # Until we can fix the GServer stopping race condition (Issue #7)
   end
 
   def quiet_logger
