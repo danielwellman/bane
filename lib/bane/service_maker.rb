32818df5e46c57d64e34c50bb34755e1747cab0e
Daniel Wellman <dan@danielwellman.com>
2013-11-07 16:14:43 -0500

    Rename instance var everything to makeables

--- lib/bane/service_maker.rb
+++ lib/bane/service_maker.rb
@@ -2,23 +2,31 @@ module Bane
 
   class ServiceMaker
     def initialize
-      @everything = {}
-      Bane::Behaviors::EXPORTED.each { |behavior| @everything[behavior.unqualified_name] = BehaviorMaker.new(behavior) }
-      Bane::Services::EXPORTED.each { |service| @everything[service.unqualified_name] = service }
+      initialize_makeables
     end
 
     def all_service_names
-      @everything.keys.sort
+      makeables.keys.sort
     end
 
     def create(service_names, starting_port, host)
       service_names
-        .map { |service_name| @everything.fetch(service_name) { raise UnknownServiceError.new(service_name) } }
+        .map { |service_name| makeables.fetch(service_name) { raise UnknownServiceError.new(service_name) } }
         .map.with_index { |maker, index| maker.make(starting_port + index, host) }
     end
 
     def create_all(starting_port, host)
       create(all_service_names, starting_port, host)
+    end
+
+    private
+
+    attr_reader :makeables
+
+    def initialize_makeables
+      @makeables = {}
+      Bane::Behaviors::EXPORTED.each { |behavior| @makeables[behavior.unqualified_name] = BehaviorMaker.new(behavior) }
+      Bane::Services::EXPORTED.each { |service| @makeables[service.unqualified_name] = service }
     end
   end
 
