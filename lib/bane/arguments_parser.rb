ccc450a14ac98fe262550864d56e5f7e3901d8be
Daniel Wellman <dan@danielwellman.com>
2013-11-14 13:55:12 -0500

    Funny extraction which creates a class which seems to mirror what the parent ArgumentsParser class did.

--- lib/bane/arguments_parser.rb
+++ lib/bane/arguments_parser.rb
@@ -4,20 +4,55 @@ module Bane
   class ArgumentsParser
     def initialize
       @service_maker = ServiceMaker.new
+      @options_parser = BaneOptionsParser.new
+    end
+
+    def parse(args)
+      arguments = @options_parser.parse(args)
+
+      return [] if arguments.empty? #TODO don't depend on mutated args
+
+      create(arguments.services, arguments.port, arguments.host)
+    end
+
+    def usage
+      @options_parser.usage
+    end
+
+    private
+
+    def create(services, port, host)
+      if services.any?
+        @service_maker.create(services, port, host)
+      else
+        @service_maker.create_all(port, host)
+      end
+    rescue UnknownServiceError => ube
+      raise ConfigurationError, ube.message
+    end
+
+  end
+
+  class ConfigurationError < RuntimeError;
+  end
+
+  class BaneOptionsParser
+    def initialize
+      @service_maker = ServiceMaker.new
       @options = {host: default_host}
       @option_parser = init_option_parser
     end
 
     def parse(args)
-      parse_options(args)
+      @option_parser.parse!(args)
 
-      return [] if (args.empty?)
+      return ParsedArguments.empty if args.empty?
 
       port = parse_port(args[0])
       services = args.drop(1)
-      host = @options[:host]
-      configuration = Configuration.new(port, host, services)
-      create(configuration.services, configuration.port, configuration.host)
+      ParsedArguments.new(port, @options[:host], services)
+    rescue OptionParser::InvalidOption => io
+      raise ConfigurationError, io.message
     end
 
     def usage
@@ -31,7 +66,7 @@ module Bane
         opts.banner = 'Usage: bane [options] port [behaviors]'
         opts.separator ''
         opts.on('-l', '--listen-on-localhost',
-          "Listen on localhost, (#{default_host}). [default]") do
+                "Listen on localhost, (#{default_host}). [default]") do
           @options[:host] = default_host
         end
         opts.on('-a', '--listen-on-all-hosts', "Listen on all interfaces, (#{all_interfaces})") do
@@ -43,26 +78,10 @@ module Bane
       end
     end
 
-    def parse_options(args)
-      @option_parser.parse!(args)
-      rescue OptionParser::InvalidOption => io
-        raise ConfigurationError, io.message
-    end
-
     def parse_port(port)
       Integer(port)
-      rescue ArgumentError
-        raise ConfigurationError, "Invalid port number: #{port}"
-    end
-
-    def create(behaviors, port, host)
-      if behaviors.any?
-        @service_maker.create(behaviors, port, host)
-      else
-        @service_maker.create_all(port, host)
-      end
-      rescue UnknownServiceError => ube
-        raise ConfigurationError, ube.message
+    rescue ArgumentError
+      raise ConfigurationError, "Invalid port number: #{port}"
     end
 
     def all_interfaces
@@ -72,10 +91,31 @@ module Bane
     def default_host
       Services::DEFAULT_HOST
     end
-
   end
 
-  class ConfigurationError < RuntimeError; end
+  class ParsedArguments
 
-  Configuration = Struct.new(:port, :host, :services)
+    attr_reader :port, :host, :services
+
+    def initialize(port, host, services)
+      @host = host
+      @port = port
+      @services = services
+    end
+
+    def empty?
+      false
+    end
+
+    def self.empty
+      EmptyArguments.new
+    end
+  end
+
+  class EmptyArguments
+    def empty?
+      true
+    end
+  end
+
 end
