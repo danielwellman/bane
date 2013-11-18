fe600d59bbd18ea0424e3c78e2729b5cd7b917b7
Daniel Wellman <dan@danielwellman.com>
2013-11-18 11:44:55 -0500

    Add tests for parsing

--- test/bane/command_line_configuration_test.rb
+++ test/bane/command_line_configuration_test.rb
@@ -50,12 +50,26 @@ class CommandLineConfigurationTest < Test::Unit::TestCase
   end
 
 
-  # Parsing the host option
+  # Parsing arguments
+
+  def test_parses_the_port
+    config = parse(["3000", IRRELEVANT_BEHAVIOR])
+    assert_equal 3000, config.port
+  end
+
+  def test_parses_the_services
+    config = parse([IRRELEVANT_PORT, 'NeverRespond', 'EchoResponse'])
+    assert_equal ['NeverRespond', 'EchoResponse'], config.services
+  end
+
+  def test_host_defaults_to_localhost_if_not_specified
+    config = parse([IRRELEVANT_PORT, IRRELEVANT_BEHAVIOR])
+    assert_equal '127.0.0.1', config.host
+  end
 
   def test_dash_l_option_sets_listen_host_to_localhost
     assert_parses_host(Services::DEFAULT_HOST, ['-l', IRRELEVANT_PORT, IRRELEVANT_BEHAVIOR])
   end
-
 
   def test_listen_on_localhost_sets_listen_host_to_localhost
     assert_parses_host(Services::DEFAULT_HOST, ['--listen-on-localhost', IRRELEVANT_PORT, IRRELEVANT_BEHAVIOR])
@@ -70,8 +84,12 @@ class CommandLineConfigurationTest < Test::Unit::TestCase
   end
 
 
+  def parse(arguments)
+    ArgumentsParser.new.parse(arguments)
+  end
+
   def assert_parses_host(expected_host, arguments)
-    config = ArgumentsParser.new.parse(arguments)
+    config = parse(arguments)
     assert_equal expected_host, config.host
   end
 
